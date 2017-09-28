98e8d23959513a09f73d97f98c2adba2f9e0912e
Evtuhov <Evtuhov@binbank.ru>
2017-09-28 23:56:14 +0300

    commit

--- API_Hardware/models.py
+++ API_Hardware/models.py
@@ -1,10 +1,9 @@
-from django.db import models
 from rat.models import *
-import datetime
+
 
 class ControlTelemetry(models.Model):
-    vehicle = models.ForeignKey(Vehicle,null=True)
-    datetime = models.DateTimeField(verbose_name='datetime',null=True)
+    vehicle = models.ForeignKey(Vehicle, null=True)
+    datetime = models.BigIntegerField(verbose_name='datetime', default=0)#datetime = models.DateTimeField(verbose_name='datetime', null=True)
     torque = models.FloatField(verbose_name='torque', default=0)
     breake = models.FloatField(verbose_name='breake', default=0)
     rpm = models.FloatField(verbose_name='rpm', default=0)
@@ -12,27 +11,22 @@ class ControlTelemetry(models.Model):
 
 class LocationTelemetry(models.Model):
     vehicle = models.ForeignKey(Vehicle, null=True)
-    datetime = models.DateTimeField(verbose_name='datetime', null=True)
+    datetime = models.BigIntegerField(verbose_name='datetime', default=0)#datetime = models.DateTimeField(verbose_name='datetime', null=True)
     spd = models.FloatField(verbose_name='spd', default=0)
     latitude = models.FloatField(verbose_name='latitude', default=0)
     longitude = models.FloatField(verbose_name='longitude', default=0)
 
+class SpeedTask(models.Model):
+    loc_telemetry = models.ForeignKey(LocationTelemetry, null=True)
+
 class Telemetry(models.Model):
     vehicle = models.ForeignKey(Vehicle, null=True)
-    datetime = models.DateTimeField(verbose_name='datetime', null=True)
+    datetime = models.BigIntegerField(verbose_name='datetime',default=0)#models.DateTimeField(verbose_name='datetime', null=True)
     fuel = models.FloatField(verbose_name='fuel', default=0)
     acc_x = models.FloatField(verbose_name='acc_x', default=0)
     acc_y = models.FloatField(verbose_name='acc_y', default=0)
     acc_z = models.FloatField(verbose_name='acc_z', default=0)
 
-#class CrashDescription(models.Model):
-#    code = models.CharField(verbose_name='code', max_length=24, default="", unique=True)
-#    full_description = models.TextField(verbose_name='full_description', default="")
-#    short_description = models.TextField(verbose_name='full_description', default="")#
-##
-#
-#class Crash(models.Model):
-#    vehicle = models.ForeignKey(Vehicle, null=True)
-#    actual = models.BooleanField(verbose_name='actual', default=True, db_index=True)
-#    description = models.ForeignKey(CrashDescription, null=True)
-#    date = models.TextField(verbose_name='crash_date', null=True)
+class SpeedExcess(models.Model):
+    loc_telemetry = models.ForeignKey(LocationTelemetry,null=True)
+    limit = models.IntegerField(verbose_name='limit', default=0)
--- API_Hardware/views.py
+++ API_Hardware/views.py
@@ -1,7 +1,14 @@
 import paho.mqtt.client as mqtt
 from API_Hardware.models import *
 import multiprocessing
+
+import random
+import time
+import calendar
+
 mqtt_listener = None
+queue = multiprocessing.Queue()
+analyzers = []
 
 
 class MessageParser:
@@ -31,33 +38,36 @@ class MessageParser:
 
     def parse_dataset_1(self):
         print("dataset_1")
-        message_date_time = datetime.datetime.utcfromtimestamp(int(self.raw_data[0]))
-        print(message_date_time)
+        #message_date_time = datetime.datetime.utcfromtimestamp(int(self.raw_data[0]))
+        #print(message_date_time)
         error_codes = self.raw_data[1:]
         print(error_codes)
         vehicle = Vehicle.objects.get(VIN=self.car_id)
 
         if vehicle is not None:
             for error in error_codes:
-                crash_description, _ = CrashDescription.objects.get_or_create(code=error)
-                if crash_description.full_description == "":
-                    crash_description.full_description = error
-                if crash_description.short_description == "":
+                if not error=="":
+                    crash_description, _ = CrashDescription.objects.get_or_create(code=error)
+                    if crash_description.full_description == "":
+                        crash_description.full_description = error
+                    if crash_description.short_description == "":
                         crash_description.short_description = error
-                crash_description.save()
+                    crash_description.save()
 
-                crash = Crash.objects.create()
-                crash.actual = True
-                crash.description = crash_description
-                crash.date = message_date_time
-                crash.vehicle = vehicle
-                crash.save()
-                print(error)
+                    crash = Crash.objects.create()
+                    crash.actual = True
+                    crash.description = crash_description
+                    crash.date = calendar.timegm(time.gmtime())
+#datetime.datetime.utcnow()#str(datetime.datetime)#message_date_time
+                    crash.vehicle = vehicle
+                    crash.save()
+                    print(error)
+
 
     def parse_dataset_2(self):
         print("dataset_2")
-        message_date_time = datetime.datetime.utcfromtimestamp(int(self.raw_data[0]))
-        print(message_date_time)
+        #message_date_time = datetime.datetime.utcfromtimestamp(int(self.raw_data[0]))
+        #print(message_date_time)
         rpm = float(int(self.raw_data[1])/3000.1)*8000
         torque = int(self.raw_data[2])/250.1
         breake = int(self.raw_data[3])/250.1
@@ -70,7 +80,8 @@ class MessageParser:
         if vehicle is not None:
             control_telemetry = ControlTelemetry.objects.create()
             control_telemetry.vehicle = vehicle
-            control_telemetry.datetime = message_date_time
+            control_telemetry.datetime = calendar.timegm(time.gmtime())
+#datetime.datetime.utcnow()#str(datetime.datetime)#datetime.datetime#message_date_time
             control_telemetry.rpm = rpm
             control_telemetry.breake = breake
             control_telemetry.torque = torque
@@ -78,11 +89,11 @@ class MessageParser:
 
     def parse_dataset_3(self):
         print("dataset_3")
-        message_date_time = datetime.datetime.utcfromtimestamp(int(self.raw_data[0]))
-        print(message_date_time)
-        spd = float(self.raw_data[2])
-        latitude = float(self.raw_data[3])
-        longitude = float(self.raw_data[4])
+        #message_date_time = datetime.datetime.utcfromtimestamp(int(self.raw_data[0]))
+        #print(message_date_time)
+        spd = float(self.raw_data[1])
+        latitude = float(self.raw_data[2])
+        longitude = float(self.raw_data[3])
         print("spd " + str(spd))
         print("latitude " + str(latitude))
         print("longetude " + str(longitude))
@@ -92,57 +103,105 @@ class MessageParser:
         if vehicle is not None:
             telemetry = LocationTelemetry.objects.create()
             telemetry.vehicle = vehicle
-            telemetry.datetime = message_date_time
+            telemetry.datetime = calendar.timegm(time.gmtime())
+#datetime.datetime.utcnow()#str(datetime.datetime)#datetime.datetime#message_date_time
             telemetry.spd = spd
             telemetry.latitude = latitude
             telemetry.longitude = longitude
             telemetry.save()
 
+            spd_task = SpeedTask.objects.create()
+            spd_task.loc_telemetry = telemetry
+            spd_task.save()
+
+            for task in SpeedTask.objects.all():
+                queue.put(task.id)
+
     def parse_dataset_4(self):
         print("dataset_4")
-        fuel = int(int(self.raw_data[1]) / 255.0)
+        #message_date_time = datetime.utcfromtimestamp(int(self.raw_data[0]))
+        #print(message_date_time)
+        fuel = float(float(self.raw_data[1]) / 255.0)
         print("fuel " + str(fuel))
-#        #accX = float(self.raw_data[2])
-#        #accY = float(self.raw_data[3])
-#        #accZ = float(self.raw_data[4])
+        acc_x = float(self.raw_data[2])
+        acc_y = float(self.raw_data[3])
+        acc_z = float(self.raw_data[4])
+        print('acc_x ' + str(acc_x))
+        print('acc_y ' + str(acc_y))
+        print('acc_z ' + str(acc_z))
+
+        vehicle = Vehicle.objects.get(VIN=self.car_id)
+
+        if vehicle is not None:
+            telemetry = Telemetry.objects.create()
+            telemetry.vehicle = vehicle
+            telemetry.datetime = calendar.timegm(time.gmtime())
+#datetime.datetime.utcnow()#str(datetime.datetime)#datetime.datetime#message_date_time
+            telemetry.fuel = fuel
+            telemetry.acc_x = acc_x
+            telemetry.acc_y = acc_y
+            telemetry.acc_z = acc_z
+            telemetry.save()
+
+
+
 
 
 def on_connect(rc):
     print("rc: " + str(rc))
 
 
-def on_message(msg):
+def on_message(q,e,msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
     try:
         MessageParser(msg.payload.decode("utf-8"))
-    except Exception:
-        print('shitty message')
+    except Exception as e:
+        print(e)
 
 
-def on_publish(mid):
-    print("mid: " + str(mid))
-
-
-def on_subscribe(mid, granted_qos):
+def on_subscribe(q,w,mid, granted_qos):
     print("Subscribed: " + str(mid) + " " + str(granted_qos))
-
-
-def on_log(string):
-    print(string)
 
 
 def setup_listener():
     mqttc = mqtt.Client()
     # Assign event callbacks
     mqttc.on_message = on_message
-    # mqttc.on_connect = on_connect
-    mqttc.on_publish = on_publish
+
     mqttc.on_subscribe = on_subscribe
     # Connect
     mqttc.connect("broker.hivemq.com", 1883, 60)
     mqttc.subscribe("mts_hardware_get_info_chanel", qos=0)
     # Continue the network loop
     mqttc.loop_forever()
+
+
+def analyzer(id):
+    while(True):
+        if(not queue.empty()):
+            print('analyzer '+ str(id) + 'start analyze')
+            id = queue.get()
+
+            # make request to api
+            limits = [5, 10, 20, 30, 40, 60, 70, 80, 90, 100, 110]
+            random.seed()
+            limit = limits[random.randrange(0, len(limits) - 1)]
+            task = SpeedTask.objects.get(id=id)
+            if (task.loc_telemetry.spd > limit):
+                speed_excess = SpeedExcess.objects.create()
+                speed_excess.loc_telemetry = task.loc_telemetry
+                speed_excess.limit = limit
+                speed_excess.save()
+                task.delete()
+        else:
+            print('analyzer '+ str(id) + 'is waiting')
+        time.sleep(5)
+
+def lauch_analyzers(count):
+    for i in range(0,count):
+        process = multiprocessing.Process(target=analyzer, args=(i,))
+        process.start()
+        analyzers.append(process)
 
 
 def lauch_listener():
@@ -152,6 +211,11 @@ def lauch_listener():
     #    print("_")
     #    time.sleep(3)
 
+
+
 print('prepare to lauch')
 if mqtt_listener is None:
-    setup_listener()  #lauch_listener()
+    #setup_listener()
+    lauch_listener()
+if len(analyzers) == 0:
+    lauch_analyzers(1)
