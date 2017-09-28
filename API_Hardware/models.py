b11a1547cb50a0a8c3a83c2579d3f5d0797e443a
Evtuhov <Evtuhov@binbank.ru>
2017-09-28 20:51:58 +0300

    next change

--- API_Hardware/models.py
+++ API_Hardware/models.py
@@ -1,23 +1,38 @@
 from django.db import models
+from rat.models import *
+import datetime
 
-class Vehicle(models.Model):
-    VIN = models.CharField(verbose_name='vin', max_length=17, default="")
-    number = models.CharField(verbose_name='number', max_length=9, default="")
-    brand = models.CharField(verbose_name='brand', max_length=255, default="")
-    model = models.CharField(verbose_name='model', max_length=255, default="")
-    year = models.CharField(verbose_name='year', max_length=4, default="")
-    user = models.ForeignKey(User, null=True)
-    is_auction = models.BooleanField(verbose_name='is_auction', default=False, db_index=True)
-
-
-class CrashDescription(models.Model):
-    code = models.CharField(verbose_name='code', max_length=24, default="", unique=True)
-    full_description = models.TextField(verbose_name='full_description', default="")
-    short_description = models.TextField(verbose_name='full_description', default="")
+class ControlTelemetry(models.Model):
+    vehicle = models.ForeignKey(Vehicle,null=True)
+    datetime = models.DateTimeField(verbose_name='datetime',null=True)
+    torque = models.FloatField(verbose_name='torque', default=0)
+    breake = models.FloatField(verbose_name='breake', default=0)
+    rpm = models.FloatField(verbose_name='rpm', default=0)
 
 
-class Crash(models.Model):
+class LocationTelemetry(models.Model):
     vehicle = models.ForeignKey(Vehicle, null=True)
-    actual = models.BooleanField(verbose_name='actual', default=True, db_index=True)
-    description = models.ForeignKey(CrashDescription, null=True)
-    date = models.TextField(verbose_name='crash_date', null=True)
+    datetime = models.DateTimeField(verbose_name='datetime', null=True)
+    spd = models.FloatField(verbose_name='spd', default=0)
+    latitude = models.FloatField(verbose_name='latitude', default=0)
+    longitude = models.FloatField(verbose_name='longitude', default=0)
+
+class Telemetry(models.Model):
+    vehicle = models.ForeignKey(Vehicle, null=True)
+    datetime = models.DateTimeField(verbose_name='datetime', null=True)
+    fuel = models.FloatField(verbose_name='fuel', default=0)
+    acc_x = models.FloatField(verbose_name='acc_x', default=0)
+    acc_y = models.FloatField(verbose_name='acc_y', default=0)
+    acc_z = models.FloatField(verbose_name='acc_z', default=0)
+
+#class CrashDescription(models.Model):
+#    code = models.CharField(verbose_name='code', max_length=24, default="", unique=True)
+#    full_description = models.TextField(verbose_name='full_description', default="")
+#    short_description = models.TextField(verbose_name='full_description', default="")#
+##
+#
+#class Crash(models.Model):
+#    vehicle = models.ForeignKey(Vehicle, null=True)
+#    actual = models.BooleanField(verbose_name='actual', default=True, db_index=True)
+#    description = models.ForeignKey(CrashDescription, null=True)
+#    date = models.TextField(verbose_name='crash_date', null=True)
--- API_Hardware/views.py
+++ API_Hardware/views.py
@@ -1,3 +1,157 @@
-from django.shortcuts import render
+import paho.mqtt.client as mqtt
+from API_Hardware.models import *
+import multiprocessing
+mqtt_listener = None
 
-# Create your views here.
+
+class MessageParser:
+    car_id = 0
+    dataset_type_id = 0
+    raw_data = []
+    raw_data_string = ""
+
+    def __init__(self, raw_data_string):
+        self.raw_data_string = raw_data_string
+        self.parse_data()
+
+    def parse_data(self):
+        raw_array = self.raw_data_string.split('_')
+        self.car_id = raw_array[0]
+        self.dataset_type_id = int(raw_array[1])
+        self.raw_data = raw_array[2:]
+
+        if self.dataset_type_id == 1:
+            self.parse_dataset_1()
+        elif self.dataset_type_id == 2:
+            self.parse_dataset_2()
+        elif self.dataset_type_id == 3:
+            self.parse_dataset_3()
+        elif self.dataset_type_id == 4:
+            self.parse_dataset_4()
+
+    def parse_dataset_1(self):
+        print("dataset_1")
+        message_date_time = datetime.datetime.utcfromtimestamp(int(self.raw_data[0]))
+        print(message_date_time)
+        error_codes = self.raw_data[1:]
+        print(error_codes)
+        vehicle = Vehicle.objects.get(VIN=self.car_id)
+
+        if vehicle is not None:
+            for error in error_codes:
+                crash_description, _ = CrashDescription.objects.get_or_create(code=error)
+                if crash_description.full_description == "":
+                    crash_description.full_description = error
+                if crash_description.short_description == "":
+                        crash_description.short_description = error
+                crash_description.save()
+
+                crash = Crash.objects.create()
+                crash.actual = True
+                crash.description = crash_description
+                crash.date = message_date_time
+                crash.vehicle = vehicle
+                crash.save()
+                print(error)
+
+    def parse_dataset_2(self):
+        print("dataset_2")
+        message_date_time = datetime.datetime.utcfromtimestamp(int(self.raw_data[0]))
+        print(message_date_time)
+        rpm = float(int(self.raw_data[1])/3000.1)*8000
+        torque = int(self.raw_data[2])/250.1
+        breake = int(self.raw_data[3])/250.1
+        print("rpm "+str(rpm))
+        print("torque " + str(torque))
+        print("breake " + str(breake))
+
+        vehicle = Vehicle.objects.get(VIN=self.car_id)
+
+        if vehicle is not None:
+            control_telemetry = ControlTelemetry.objects.create()
+            control_telemetry.vehicle = vehicle
+            control_telemetry.datetime = message_date_time
+            control_telemetry.rpm = rpm
+            control_telemetry.breake = breake
+            control_telemetry.torque = torque
+            control_telemetry.save()
+
+    def parse_dataset_3(self):
+        print("dataset_3")
+        message_date_time = datetime.datetime.utcfromtimestamp(int(self.raw_data[0]))
+        print(message_date_time)
+        spd = float(self.raw_data[2])
+        latitude = float(self.raw_data[3])
+        longitude = float(self.raw_data[4])
+        print("spd " + str(spd))
+        print("latitude " + str(latitude))
+        print("longetude " + str(longitude))
+
+        vehicle = Vehicle.objects.get(VIN=self.car_id)
+
+        if vehicle is not None:
+            telemetry = LocationTelemetry.objects.create()
+            telemetry.vehicle = vehicle
+            telemetry.datetime = message_date_time
+            telemetry.spd = spd
+            telemetry.latitude = latitude
+            telemetry.longitude = longitude
+            telemetry.save()
+
+    def parse_dataset_4(self):
+        print("dataset_4")
+        fuel = int(int(self.raw_data[1]) / 255.0)
+        print("fuel " + str(fuel))
+#        #accX = float(self.raw_data[2])
+#        #accY = float(self.raw_data[3])
+#        #accZ = float(self.raw_data[4])
+
+
+def on_connect(rc):
+    print("rc: " + str(rc))
+
+
+def on_message(msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    try:
+        MessageParser(msg.payload.decode("utf-8"))
+    except Exception:
+        print('shitty message')
+
+
+def on_publish(mid):
+    print("mid: " + str(mid))
+
+
+def on_subscribe(mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+def on_log(string):
+    print(string)
+
+
+def setup_listener():
+    mqttc = mqtt.Client()
+    # Assign event callbacks
+    mqttc.on_message = on_message
+    # mqttc.on_connect = on_connect
+    mqttc.on_publish = on_publish
+    mqttc.on_subscribe = on_subscribe
+    # Connect
+    mqttc.connect("broker.hivemq.com", 1883, 60)
+    mqttc.subscribe("mts_hardware_get_info_chanel", qos=0)
+    # Continue the network loop
+    mqttc.loop_forever()
+
+
+def lauch_listener():
+    listner = multiprocessing.Process(target=setup_listener, args=())
+    listner.start()
+    # while(True):
+    #    print("_")
+    #    time.sleep(3)
+
+print('prepare to lauch')
+if mqtt_listener is None:
+    setup_listener()  #lauch_listener()
