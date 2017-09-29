aab6fbcc3392e5395a40b8b0bb0a5a6fa8d968f2
leoniknik <leoniknik@mail.ru>
2017-09-29 11:39:26 +0300

    https

--- rat/views.py
+++ rat/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rat.models import User, Vehicle, Crash, CrashDescription, Offer, Service, Review
+import json
+from django.http import JsonResponse, HttpResponse, HttpResponseForbidden, HttpResponseBadRequest
+from django.core import serializers
 
-# Create your views here.
+def user_login(request):
+    try:
+        json_data = json.loads(str(request.body, encoding='utf-8'))
+        print(json_data)
+        email = json_data['email']
+        user = User.objects.filter(email=email).first()
+        if user is None:
+            return JsonResponse({"code": 403})
+        else:
+            return JsonResponse({"code": 200, "data": {
+                }})
+    except Exception as e:
+        print(e)
+        return JsonResponse({"code": 403})
